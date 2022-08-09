0182fada5911c8ff21def902cb87d12755a43d64
lidashuang <1294935733@qq.com>
2022-08-09 09:39:15 +0800

    3

--- p6e-image-encoder/src/main/java/club.p6e.image.encoder/Actuator.java
+++ p6e-image-encoder/src/main/java/club.p6e.image.encoder/Actuator.java
@@ -1,10 +1,45 @@
 package club.p6e.image.encoder;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.security.Provider;
+import java.security.SecureRandom;
+
 /**
  * @author lidashuang
  * @version 1.0
  */
-public interface Actuator {
+public abstract class Actuator {
 
-    public void execute(String secret, Integer remark);
+    private static final int KEY_SIZE = 128;
+    private static final String KEY_ALGORITHM = "AES";
+    private static final String SECURE_RANDOM_ALGORITHM = "SHA1PRNG";
+    private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
+
+    public SecretKey getSecretKey(String secret) throws Exception {
+        final KeyGenerator keyGenerator = KeyGenerator.getInstance(KEY_ALGORITHM);
+        SecureRandom random = SecureRandom.getInstance(SECURE_RANDOM_ALGORITHM);
+        random.setSeed(secret.getBytes(StandardCharsets.UTF_8));
+        keyGenerator.init(KEY_SIZE, random);
+        return keyGenerator.generateKey();
+    }
+
+
+    public byte[] encrypt(byte[] data, String secret) throws Exception {
+        final Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(secret));
+        return cipher.doFinal(data);
+    }
+
+    public byte[] decrypt(byte[] data, String secret) throws Exception {
+        final Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, getSecretKey(secret));
+        return cipher.doFinal(data);
+    }
+
+    public abstract void execute(String secret, Integer remark);
 }
--- p6e-image-encoder/src/main/java/club.p6e.image.encoder/FileActuator.java
+++ p6e-image-encoder/src/main/java/club.p6e.image.encoder/FileActuator.java
@@ -1,15 +1,21 @@
 package club.p6e.image.encoder;
 
+import org.apache.commons.codec.binary.StringUtils;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.channels.FileChannel;
 
 /**
  * @author lidashuang
  * @version 1.0
  */
-public abstract class FileActuator implements Actuator {
+public abstract class FileActuator extends Actuator {
 
     private final File file;
 
@@ -20,12 +26,28 @@ public abstract class FileActuator implements Actuator {
     @Override
     public void execute(String secret, Integer remark) {
         FileInputStream fileInputStream = null;
+        FileChannel fileChannel = null;
         try {
             fileInputStream = new FileInputStream(this.file);
-            FileChannel fileChannel = fileInputStream.getChannel();
+            fileChannel = fileInputStream.getChannel();
             fileChannel.read();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
+        } finally {
+            try {
+                if (fileChannel != null) {
+                    fileChannel.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
